9d8a0549ad6f47d5e7c5d0e4b867bbe42f9ea123
cristian-rincon <cristian.o.rincon.b@gmail.com>
2021-01-02 23:19:05 -0500

    se crea primera vesion del frontend para la todo app

---
+++ py/webapps/todoapp/frontend/src/mock/todos.js
@@ -0,0 +1,35 @@
+const todoItems = [
+    {
+        "id": 1,
+        "title": "Crear una standalone app (Django)",
+        "description": null,
+        "completed": false
+    },
+    {
+        "id": 2,
+        "title": "Crear una todoApp simple con Django rest framework",
+        "description": null,
+        "completed": false
+    },
+    {
+        "id": 3,
+        "title": "Crear pruebas de la todoApp via postman",
+        "description": null,
+        "completed": false
+    },
+    {
+        "id": 4,
+        "title": "Cambiar los métodos de las class based views por mixins",
+        "description": null,
+        "completed": true
+    },
+    {
+        "id": 5,
+        "title": "Nueva tarea",
+        "description": "Esta es una nueva tarea",
+        "completed": false
+    }
+]
+
+
+export default todoItems;
